16a73662e51aeb27508fcb045bd8cd808a59e820
davidlin <aunbdaunbd@gmail.com>
2019-05-21 20:37:16 +0800

    < Project 02 > Part A
    Correctly allocate memory for cache and setup LRU head.
    
    Description:
        LRU head point to the cache entry that is least recently
        used. When a cache miss occurs, it'll swap out the  data
        in the entry LRU head points to.

--- Project02/Part_A/csim.c
+++ Project02/Part_A/csim.c
@@ -13,11 +13,13 @@
 static uint64_t BLOCK_BITS;
 // Index
 static uint64_t INDEX_BITS;
-#define INDEX_MASK ((1UL << INDEX_BITS) - 1)
+#define ENTRY_NUM (1UL << INDEX_BITS)
+#define INDEX_MASK (ENTRY_NUM - 1)
 #define INDEX_OFFSET (BLOCK_BITS)
 #define GET_ADDR_INDEX(addr) __GET_VAL(addr, INDEX_OFFSET, INDEX_MASK)
 // Way
 static uint64_t WAY_BITS;
+#define WAY_NUM (1UL << WAY_BITS)
 // Tag
 #define TAG_BITS (ADDR_BITS - INDEX_BITS - BLOCK_BITS)
 #define TAG_MASK ((1UL << TAG_BITS) - 1)
@@ -31,23 +33,54 @@ static uint64_t WAY_BITS;
 /* File name. */
 static char *FILE_NAME;
 
-#define INVALID_INPUT { printf("Invalid input!!\n"); return -1; }
+struct cache_entry_struct {
+	uint64_t cache_entry;
+	struct cache_entry_t *prev;
+	struct cache_entry_t *next;
+};
+#define cache_entry_t struct cache_entry_struct
+
+static inline void invalid_input() {
+	fprintf(stderr, "Invalid input!!\n");
+	exit(1);
+}
 
 int main(int argc, char** argv)
 {
+	cache_entry_t **cache, **lru_head;
 	char ch;
 
 	/* Get inputs. */
-	if (argc != 9) INVALID_INPUT
+	if (argc != 9) invalid_input();
 	// Get number of set index bits.
 	if ((ch = getopt(argc, argv, "s:")) != -1) INDEX_BITS = atoi(optarg);
-	else INVALID_INPUT
+	else invalid_input();
 	if ((ch = getopt(argc, argv, "E:")) != -1) WAY_BITS = atoi(optarg);
-	else INVALID_INPUT
+	else invalid_input();
 	if ((ch = getopt(argc, argv, "b:")) != -1) BLOCK_BITS = atoi(optarg);
-	else INVALID_INPUT
+	else invalid_input();
 	if ((ch = getopt(argc, argv, "t:")) != -1) FILE_NAME = optarg;
-	else INVALID_INPUT
+	else invalid_input();
+
+	/* Allocate cache memory. */
+	cache = (cache_entry_t **)malloc(sizeof(cache_entry_t) * ENTRY_NUM);
+	lru_head = (cache_entry_t **)malloc(sizeof(cache_entry_t) * ENTRY_NUM);
+	for (int i = 0; i < ENTRY_NUM; i++) {
+		cache[i] = (cache_entry_t *)malloc(sizeof(cache_entry_t) * WAY_NUM);
+		// Initialize cache entries.
+		for (int j = 0; j < WAY_NUM; j++) {
+			cache[i]->prev = NULL;
+			cache[i]->next = NULL;
+		}
+		lru_head[i] = NULL;
+	}
+
+	/* Free cache memory. */
+	for (int i = 0; i < ENTRY_NUM; i++) {
+		free(cache[i]);
+	}
+	free(cache);
+	free(lru_head);
 
     printSummary(0, 0, 0);
     return 0;
